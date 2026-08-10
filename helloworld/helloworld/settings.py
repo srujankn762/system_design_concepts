efc20e08d3cbc0bd4f0a415770a71fd0c486ef4d
srujankn762 <srujangowda762@gmail.com>
2026-08-10 18:44:39 +0530

    feat: Added migration

--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "USER": os.getenv("DB_USER", "postgres"),
         "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
         "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "PORT": os.getenv("DB_PORT", "5445"),
     }
 }
 
