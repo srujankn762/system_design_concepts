586d13029c512de80605601c53f5ec6c73f6a52f
srujankn762 <srujangowda762@gmail.com>
2026-08-10 21:50:01 +0530

    feat: Added dummy API

--- helloworld/api/views.py
+++ helloworld/api/views.py
@@ -1,8 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Employee
 from .serializers import EmployeeSerializer
+from django.http import JsonResponse
 
 
 class EmployeeViewSet(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+
+def hello_world(request):
+    return JsonResponse({
+        "message": "Hello World!",
+        "status": "success"
+    })
